89ffd1577b74a9696fd26cf5123d76ec372812dd
roushan-sheik <roushansheik@gmail.com>
2024-08-25 19:54:51 +0600

    final ready to deploy

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express";
-import config from "./config";
+import express, { Request, Response } from "express";
 
 const app = express();
 // cors options
 const options = {
-  origin: config.cors_origin, 
+  origin: config.cors_origin,
   credentials: true,
 };
 // middlewares
@@ -15,6 +14,10 @@ app.use(cors(options));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+// home route
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ success: true, message: "Movie Home Route" });
+});
 
 // route import
 import routes from "./routes";
@@ -22,6 +25,7 @@ app.use("/api/v1", routes.movieRoute);
 app.use("/api/v1/movies", routes.reviewRoute);
 
 // global error handler and not found middleware
+import config from "./config";
 import { handleGlobalError, notFound } from "./middlewares";
 app.use(notFound);
 app.use(handleGlobalError);
