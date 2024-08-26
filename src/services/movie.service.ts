548784d10584712e51fa5b7b6f5d023ec5b0c0ef
roushan-sheik <roushansheik@gmail.com>
2024-08-26 11:17:15 +0600

    searching filtering and pagination added

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -10,7 +10,7 @@ const createMovie = async (payload: TMovie) => {
 };
 // get all movies
 const getAllMovies = async (payload: Record<string, unknown>) => {
-  // searching
+  //* searching ==================================>
   let searchTerm = "";
   if (payload?.searchTerm) {
     searchTerm = payload.searchTerm as string;
@@ -21,11 +21,27 @@ const getAllMovies = async (payload: Record<string, unknown>) => {
       [field]: { $regex: searchTerm, $options: "i" },
     })),
   });
-  // filtering
+  //* pagination ===================================>
+  // 1st  skip = 0
+  // 2nd skip = 2*10 - 1*10
+  // 3rd skip = 3*10 - 2*10
+  // skip = page-1 * limit
+  const limit: number = Number(payload?.limit || 10);
+  let skip: number = 0;
+  if (payload?.page) {
+    const page: number = Number(payload?.page || 1);
+    skip = Number((page - 1) * limit);
+  }
+  const skipQuery = searchedMovies.skip(skip);
+  const limitQuery = skipQuery.limit(limit);
+
+  //* filtering ===================================>
   const payloadObj = { ...payload };
-  const excludeField = ["searchTerm"];
+  const excludeField = ["searchTerm", "limit", "page"];
   excludeField.forEach((filed) => delete payloadObj[filed]);
-  const result = await searchedMovies.find(payloadObj);
+
+  const result = await limitQuery.find(payloadObj);
+
   return result;
 };
 // get single movie by id
