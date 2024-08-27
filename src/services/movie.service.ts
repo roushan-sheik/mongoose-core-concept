428a29e7e00d8ebaeea55c02540c0d9551e7b6fd
roushan-sheik <roushansheik@gmail.com>
2024-08-27 15:53:27 +0600

    query handling added with efficient way

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,3 +1,4 @@
+import { QueryBuilder } from "../builder/queryBuilder";
 import { TMovie } from "../interfaces";
 import { Movie } from "../models/movie.model";
 
@@ -10,41 +11,12 @@ const createMovie = async (payload: TMovie) => {
 };
 // get all movies
 const getAllMovies = async (payload: Record<string, unknown>) => {
-  //* 1. searching ==================================>
-  let searchTerm = "";
-  if (payload?.searchTerm) {
-    searchTerm = payload.searchTerm as string;
-  }
-  const searchableFields = ["title", "genre"];
-  const searchedMovies = Movie.find({
-    $or: searchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: "i" },
-    })),
-  });
-  //* 2. pagination ===================================>
-  const limit: number = Number(payload?.limit || 10);
-  let skip: number = 0;
-  if (payload?.page) {
-    const page: number = Number(payload?.page || 1);
-    skip = Number((page - 1) * limit);
-  }
-  const skipQuery = searchedMovies.skip(skip);
-  const limitQuery = skipQuery.limit(limit);
-
-  //* 3. Sorting ========================================>
-  let sortBy = "-releaseDate";
-  if (payload?.sortBy) {
-    sortBy = payload.sortBy as string;
-  }
-  const sortedQuery = limitQuery.sort(sortBy);
-  //* 4. filtering ===================================>
-  const payloadObj = { ...payload };
-  const excludeField = ["searchTerm", "limit", "page", "sortBy"];
-  excludeField.forEach((filed) => delete payloadObj[filed]);
-
-  const result = await sortedQuery.find(payloadObj);
-  // http://localhost:8000/api/v1/movies?searchTerm=arifa&releaseDate=2000-01-16&page=1&limit=10&sortBy=releaseDate
-
+  const movieQuery = new QueryBuilder(Movie.find({}), payload)
+    .search(["genre", "title", "limit", "page"])
+    .paginate()
+    .sort()
+    .filter();
+  const result = await movieQuery.modelQuery;
   return result;
 };
 // get single movie by id
